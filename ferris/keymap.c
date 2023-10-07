79f57a4fadbde8b2861df3cca4dca8a244c29632
Ferencz Dávid <ferencz.david@protonmail.com>
2023-10-07 22:42:59 +0200

    feat: adjust keymap

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -6,53 +6,59 @@
 // https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_hungarian.h
 #include "keymap_extras/keymap_hungarian.h"
 
+enum layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
+
 enum custom_keycodes {
     KC_PRVWD = SAFE_RANGE,
     KC_NXTWD,
     KC_LSTRT,
     KC_LEND,
-    KC_DLINE
+    KC_DLINE,
+    KC_REDO
 };
 
-#define KM_LOW_TAB  LT(1, KC_TAB)
-#define KM_RSE_BSP  LT(2, KC_BSPC)
+#define KM_LOW_TAB  LT(_LOWER, KC_TAB)
+#define KM_RSE_BSP  LT(_RAISE, KC_BSPC)
 #define KM_LGUI_SPC LGUI_T(KC_SPC)
-#define KM_LALT_S   LALT_T(KC_S)
-#define KM_LSFT_D   LSFT_T(KC_D)
-#define KM_LCTL_F   LCTL_T(KC_F)
-#define KM_RCTL_J   RCTL_T(KC_J)
-#define KM_RSFT_K   RSFT_T(KC_K)
-#define KM_RALT_L   LALT_T(KC_L)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-    KC_Q        , KC_W      , KC_E      , KC_R      , KC_T      ,       KC_Y        , KC_U      , KC_I      , KC_O      , KC_P,
-    KC_A        , KM_LALT_S , KM_LSFT_D , KM_LCTL_F , KC_G      ,       KC_H        , KM_RCTL_J , KM_RSFT_K , KM_RALT_L , HU_COLN,
-    KC_Z        , KC_X      , KC_C      , KC_V      , KC_B      ,       KC_N        , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH,
-                                          KM_LOW_TAB, KM_LGUI_SPC,      KC_ENT      , KM_RSE_BSP
+  [_QWERTY] = LAYOUT(
+    KC_Q        , KC_W          , KC_E           , KC_R        , KC_T         ,       KC_Y        , KC_U            , KC_I           , KC_O          , KC_P,
+    KC_A        , LALT_T(KC_S)  , LSFT_T(KC_D)   , LCTL_T(KC_F), KC_G         ,       KC_H        , RCTL_T(KC_J)    , RSFT_T(KC_K)   , LALT_T(KC_L)  , HU_COLN,
+    KC_Z        , KC_X          , KC_C           , KC_V        , KC_B         ,       KC_N        , KC_M            , KC_COMM        , KC_DOT        , KC_SLSH,
+                                                   KM_LOW_TAB  , KM_LGUI_SPC  ,       KC_ENT      , KM_RSE_BSP
   ),
 
-  [1] = LAYOUT(
-    KC_ESC      , HU_SLSH   , HU_GRV    , HU_QUOT   , HU_DQUO   ,       HU_PIPE     , HU_UACU   , HU_IACU   , HU_OACU   , HU_ODAC,
-    HU_LABK     , HU_EQL    , HU_LBRC   , HU_LPRN   , HU_LCBR   ,       HU_HASH     , HU_AACU   , HU_EACU   , HU_ODIA   , HU_UDIA,
-    HU_RABK     , HU_AMPR   , HU_RBRC   , HU_RPRN   , HU_RCBR   ,       HU_DLR      , HU_AT     , HU_EURO   , HU_TILD   , HU_UDAC,
-                                          _______   , _______   ,       _______     , _______
+  [_LOWER] = LAYOUT(
+    KC_ESC      , HU_SLSH       , HU_GRV         , HU_QUOT        , HU_DQUO   ,       HU_PIPE     , HU_UACU         , HU_IACU        , HU_OACU        , HU_ODAC,
+    HU_LABK     , LALT_T(HU_EQL), LSFT_T(HU_LBRC), LCTL_T(HU_LPRN), HU_LCBR   ,       HU_HASH     , RCTL_T(HU_AACU) , RSFT_T(HU_EACU), LALT_T(HU_ODIA), HU_UDIA,
+    HU_RABK     , HU_AMPR       , HU_RBRC        , HU_RPRN        , HU_RCBR   ,       HU_DLR      , HU_AT           , HU_EURO        , HU_TILD        , HU_UDAC,
+                                                   _______        , _______   ,       _______     , _______
+  ),
+  
+  [_RAISE] = LAYOUT(
+    KC_ESC      , HU_7          , HU_8           , HU_9           , HU_ASTR   ,       KC_PGUP     , KC_PRVWD  , KC_UP     , KC_NXTWD  , KC_DLINE,
+    XXXXXXX     , LALT_T(HU_4)  , LSFT_T(HU_5)   , LCTL_T(HU_6)   , HU_PLUS   ,       KC_PGDN     , KC_LEFT   , KC_DOWN   , KC_RGHT   , KC_DEL,
+    HU_0        , HU_1          , HU_2           , HU_3           , HU_PERC   ,       KC_PSCR     , KC_LSTRT  , KC_UNDO   , KC_LEND   , KC_BSPC,
+                                                   _______        , _______   ,       _______     , _______
   ),
 
-  [2] = LAYOUT(
-    KC_ESC      , HU_7      , HU_8      , HU_9      , HU_ASTR   ,       KC_PGUP     , KC_PRVWD  , KC_UP     , KC_NXTWD  , KC_DLINE,
-    _______     , HU_4      , HU_5      , HU_6      , HU_PLUS   ,       KC_PGDN     , KC_LEFT   , KC_DOWN   , KC_RGHT   , KC_DEL,
-    HU_0        , HU_1      , HU_2      , HU_3      , HU_PERC   ,       KC_PSCR     , KC_LSTRT  , XXXXXXX   , KC_LEND   , XXXXXXX,
-                                          _______   , _______   ,       _______     , _______
-  ),
-
-  [3] = LAYOUT(
-    KC_ESC      , KC_F7     , KC_F8     , KC_F9     , _______   ,       _______     , _______   , _______   , _______   , _______,
-    _______     , KC_F4     , KC_F5     , KC_F6     , _______   ,       _______     , KC_VOLD   , KC_MUTE   , KC_VOLU   , _______,
-    _______     , KC_F1     , KC_F2     , KC_F3     , _______   ,       _______     , KC_MPRV   , KC_MPLY   , KC_MNXT   , _______,
+  [_ADJUST] = LAYOUT(
+    KC_ESC      , KC_F7     , KC_F8     , KC_F9     , XXXXXXX   ,       XXXXXXX     , KC_MPRV   , KC_MPLY   , KC_MNXT   , XXXXXXX,
+    XXXXXXX     , KC_F4     , KC_F5     , KC_F6     , XXXXXXX   ,       XXXXXXX     , KC_VOLD   , KC_MUTE   , KC_VOLU   , XXXXXXX,
+    XXXXXXX     , KC_F1     , KC_F2     , KC_F3     , XXXXXXX   ,       XXXXXXX     , XXXXXXX   , KC_REDO   , XXXXXXX   , XXXXXXX,
                                           _______   , _______   ,       _______     , _______
   ),
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
 
 // https://docs.qmk.fm/#/feature_key_overrides
 const key_override_t dot_shift_exclm_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, HU_EXLM);
@@ -168,6 +174,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 unregister_mods(mod_config(MOD_LCTL));
                 unregister_code(KC_BSPC);
+            }
+            return false;
+        case KC_UNDO:
+            if (record->event.pressed) {
+                register_mods(mod_config(MOD_LCTL));
+                register_code(KC_Z);
+            } else {
+                unregister_mods(mod_config(MOD_LCTL));
+                unregister_code(KC_Z);
+            }
+            return false;
+        case KC_REDO:
+            if (record->event.pressed) {
+                register_mods(mod_config(MOD_LCTL));
+                register_mods(mod_config(MOD_LSFT));
+                register_code(KC_Z);
+            } else {
+                unregister_mods(mod_config(MOD_LCTL));
+                unregister_mods(mod_config(MOD_LSFT));
+                unregister_code(KC_Z);
             }
             return false;
         default:
